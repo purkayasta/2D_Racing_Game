2125f1219db5120c03fd94628492365bd675f039
Sunny <pritompurkayasta@gmail.com>
2017-09-02 15:09:25 +0600

    Random Car With Collusion Added !

--- Assets/Scripts/carController.cs
+++ Assets/Scripts/carController.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class carController : MonoBehaviour {
-
+public class carController : MonoBehaviour 
+{
     public float carSpeed;
     Vector3 carPosition;
     public float limitBoundary = 2.14f;
-	void Start () {
+	void Start () 
+    {
         carPosition = transform.position;
 	}
-	void Update () {
+	void Update () 
+    {
         carPosition.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
         carPosition.x = Mathf.Clamp(carPosition.x, -limitBoundary, limitBoundary);
         transform.position = carPosition;
 	}
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+            
+    }﻿
 }
--- Assets/Scripts/carSpawner.cs
+++ Assets/Scripts/carSpawner.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class carSpawner : MonoBehaviour {
 
-    public GameObject car;
+    public GameObject[] cars;
+    int carNo;
     public float limitBoundary = 2.14f;
     float timer;
-    float delayTimer = 2f;
+    public float delayTimer = 2f;
 	void Start () {
         timer = delayTimer;
 	}
 	void Update () {
         timer -= Time.deltaTime;
-
+        carNo = Random.Range(0, 5);
         if (timer <= 0)
         {
             Vector3 enemyCarPos = new Vector3(Random.Range(-limitBoundary, limitBoundary), transform.position.y, transform.position.z);
-            Instantiate(car, enemyCarPos, transform.rotation);
+            Instantiate(cars[carNo], enemyCarPos, transform.rotation);
             timer = delayTimer;
         }
        
